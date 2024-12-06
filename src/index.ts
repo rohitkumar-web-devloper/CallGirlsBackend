f6487e038e0f755edc08ec7d96a82ed990b79e0c
Rohit Kumar <rohitkumar7409137159@gmail.com>
2024-12-06 22:25:22 +0530

    correction

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import app from './app';
-import {PORT} from './constants/Variables';
+import { PORT } from './constants/Variables';
 import requireDir from 'require-dir';
-import {expressMiddleware} from '@apollo/server/express4';
+import { expressMiddleware } from '@apollo/server/express4';
 import createApolloGraphQlServer from './Graphql';
 import authMiddleware from './Middleware/atuh';
+import cors from 'cors'
+app.use(cors({
+  origin: "*", // Replace with the allowed origin(s)
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  credentials: true // Allow credentials (cookies, etc.)
+}));
 
 // Load routes and controllers
 requireDir("./routes");
@@ -14,16 +21,16 @@ app.get('/', (req, res) => {
   res.send('<center><h2>Welcome to Developer world...Ram</h2></center>');
 });
 app.get('/dude', (req, res) => {
-  res.json({type: 'success', name: 'Rohit'});
+  res.json({ type: 'success', name: 'Rohit' });
 });
 // GraphQL connection setup
 const startServer = async () => {
   app.use(
     '/graphql',
     expressMiddleware(await createApolloGraphQlServer(), {
-      context: async ({req}) => {
+      context: async ({ req }) => {
         const user = authMiddleware(req);
-        return {user};
+        return { user };
       },
     }),
   );
