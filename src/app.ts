cb165dbfefd5dab26084141435dc362e848c2e71
Rohit Kumar <rohitkumar7409137159@gmail.com>
2024-12-10 23:44:37 +0530

    Image UPload enable

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import morgan from "morgan";
+// import morgan from "morgan";
 import path from "path";
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("assets"));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 export default app;
 
 
