e2fe4faff921a4e9167eb9a01c70151577b452dd
Rohit Kumar <rohitkumar7409137159@gmail.com>
2025-01-30 23:08:08 +0530

    Order changes in some apis

--- src/Graphql/Web/ModalData/resolver.ts
+++ src/Graphql/Web/ModalData/resolver.ts
@@ -1,6 +1,8 @@
 import { ApolloError } from "apollo-server-express";
 import db from "../../../models";
 import { IResolvers } from "@graphql-tools/utils";
+import Ads from '../Ads/resolver';
+import { Op } from 'sequelize';
 const ModalData: IResolvers<any, any> = {
     Query: {
         modalCategory: async (_: any, data: any, context: any) => {
@@ -18,24 +20,56 @@ const ModalData: IResolvers<any, any> = {
             if (!user) {
                 throw new ApolloError("Unauthorized", "Unauthorized");
             }
-
-            const res:any = await db.Plan.findAll({
-                where: { status: true },
-                include: [
-                    {
-                        model: db.PlanSlot,
-                        as: 'timeSlots',
-                        attributes: ['planId', 'timeSlotId'],
-                        include: [
-                            {
-                                model: db.TimeSlots, 
-                                as: 'slots', 
-                                attributes: ['startTime', 'endTime'],
-                            }
-                        ]
+            const exist = await db.Ads.findOne({
+                where: {
+                    createdById: user.id,
+                    price: 0
+                }
+            })
+            let res: any;
+            if (exist) {
+                res = await db.Plan.findAll({
+                    where: {
+                        status: true,
+                        price: {
+                            [Op.ne]: 0,
+                        },
                     },
-                ],
-            });
+                    include: [
+                        {
+                            model: db.PlanSlot,
+                            as: 'timeSlots',
+                            attributes: ['planId', 'timeSlotId'],
+                            include: [
+                                {
+                                    model: db.TimeSlots,
+                                    as: 'slots',
+                                    attributes: ['startTime', 'endTime'],
+                                }
+                            ]
+                        },
+                    ],
+                });
+            } else {
+                res = await db.Plan.findAll({
+                    where: { status: true },
+                    include: [
+                        {
+                            model: db.PlanSlot,
+                            as: 'timeSlots',
+                            attributes: ['planId', 'timeSlotId'],
+                            include: [
+                                {
+                                    model: db.TimeSlots,
+                                    as: 'slots',
+                                    attributes: ['startTime', 'endTime'],
+                                }
+                            ]
+                        },
+                    ],
+                });
+            }
+
             return res
         },
         AdsOnCat: async (_: any, data: any, context: any) => {
