fc0eeaa8f0cfda213f2c89429bcf47bc9ae5d239
TusharGola28 <tushar.gola@ticketsque.com>
2025-02-23 11:58:50 +0530

    add

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import { Request, Response } from 'express';
 import { Sequelize } from 'sequelize';
 import db from './models';
 app.use(cors({
-  origin: ['https://lustloungh.com' , 'https://react.lustloungh.com' ,'https://test-next.lustloungh.com'],
+  origin: ['https://lustloungh.com' , 'https://react.lustloungh.com' ,'https://test-next.lustloungh.com','http://localhost:5173','http://localhost:3001'],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
